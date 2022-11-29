1275be4ec99b484cfc0975a2222ab633b8e68aec
schmulz eshiet <wisdomeshiet65@gmail.com>
2022-11-29 18:57:14 +0100

    created footer for homepage

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-[#00312d] text-white h-72  flex justify-center content-center'>
+      <div className="my-auto text-center">
+        <p className="capitalize">copyright © falcon logistics 2022. all right reserved</p>
+        <p className="capitalize mt-5">designed & inspired by timilehin bodunde</p> 
+      </div>
+  </div>
+  )
+}
+
+export default Footer
