fd1207438331159877f055d471b8813d28aa0363
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-05-30 12:06:37 +0100

    data for accordioncomponents

---
+++ src/data/accordion.ts
@@ -0,0 +1,22 @@
+export const data = [
+    {
+      service: "transparent pricing",
+      solution: "Lorem ipsum dolor sit amet, an timeam impetus viderer.",
+      text: "Sed nec mollis quam. Cras rhoncus efficitur justo. Fusce ac rutrum est, in convallis turpis. Phasellus a enim et felis semper sodar. Sed rutrum lacinia leo.",
+    },
+    {
+      service: "warehouse storage ",
+      solution: "Lorem ipsum dolor sit amet, an timeam impetus viderer.",
+      text: "Sed nec mollis quam. Cras rhoncus efficitur justo. Fusce ac rutrum est, in convallis turpis. Phasellus a enim et felis semper sodales eu a est. rutrum lacinia leo.",
+    },
+    {
+      service: "real-time tracking",
+      solution: "Lorem ipsum dolor sit amet, an timeam impetus viderer.",
+      text: "Sed nec mollis quam. Cras rhoncus efficitur justo. Fusce ac rutrum est, in convallis turpis. Phasellus a enim et felis semper sodus gravida pulvinar. Sed rutrum lacinia leo.",
+    },
+    {
+      service: "security for cargo",
+      solution: "Lorem ipsum dolor sit amet, an timeam impetus viderer.",
+      text: "Sed nec mollis quam. Cras rhoncus efficitur justo. Fusce ac rutrum est, in convallis turpis. Phasellus a enim et felis semper sodales eu a es. Sed rutrum lacinia leo.",
+    },
+  ];
