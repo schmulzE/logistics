ec5898ec22cabe15ed895a57c33ef63308730ddc
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-05-30 12:08:35 +0100

    data for tab component

---
+++ src/data/tab.ts
@@ -0,0 +1,45 @@
+export const TabList = [
+    {
+      id: 1,
+      name: 'Air Freight',
+      image: 'https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/main-home-tabbed-info-1.png',
+      title: 'Secure courier for small & big packages',
+      text: 'Lorem ipsum dolor sit amet, id has nihil discere vocibus. No novum cu labor probatus cum, te utamur scripta ei legat cum qui ei sapere quo noster sum periculis ad.'
+    },
+    {
+      id: 2,
+      name: 'Rail Freight',
+      image: 'https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/main-home-tabbed-info-2.jpg',
+      title: 'Secure courier for small & big packages',
+      text: 'Lorem ipsum dolor sit amet, id has nihil discere vocibus. No novum cu labor probatus cum, te utamur scripta ei legat cum qui ei sapere quo noster sum periculis ad.'
+    },
+    {
+      id: 3,
+      name: 'LTL Freight',
+      image: 'https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/main-home-tabbed-info-3.jpg',
+      title: 'Secure courier for small & big packages',
+      text: 'Lorem ipsum dolor sit amet, id has nihil discere vocibus. No novum cu labor probatus cum, te utamur scripta ei legat cum qui ei sapere quo noster sum periculis ad.'
+    },
+    {
+      id: 4,
+      name: 'Sea Freight',
+      image: 'https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/main-home-tabbed-info-4.jpg',
+      title: 'Secure courier for small & big packages',
+      text: 'Lorem ipsum dolor sit amet, id has nihil discere vocibus. No novum cu labor probatus cum, te utamur scripta ei legat cum qui ei sapere quo noster sum periculis ad.'
+    },
+    {
+      id: 5,
+      name: 'Land Freight',
+      image: 'https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/main-home-tabbed-info-5.jpg',
+      title: 'Secure courier for small & big packages',
+      text: 'Lorem ipsum dolor sit amet, id has nihil discere vocibus. No novum cu labor probatus cum, te utamur scripta ei legat cum qui ei sapere quo noster sum periculis ad.'
+    },
+    {
+      id: 6,
+      name: 'Warehousing',
+      image: 'https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/main-home-tabbed-info-6.jpg',
+      title: 'Secure courier for small & big packages',
+      text: 'Lorem ipsum dolor sit amet, id has nihil discere vocibus. No novum cu labor probatus cum, te utamur scripta ei legat cum qui ei sapere quo noster sum periculis ad.'
+    },
+  ]
+
