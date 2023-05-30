0e2a56f1943df6a6b2e77a9e7f0512cf2f742efd
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-05-30 12:07:25 +0100

    data for blog page

---
+++ src/data/post.ts
@@ -0,0 +1,52 @@
+export const posts = [
+    {
+      id: "1",
+      img: "https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/blog-img6.jpg",
+      date: "August 6, 2021",
+      category: "Construction",
+      title: "Planning Point Of View",
+      text: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi!",
+    },
+    {
+      id: "2",
+      img: "https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/blog-img6.jpg",
+      date: "August 6, 2021",
+      category: "Construction",
+      title: "Planning Point Of View",
+      text: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi!",
+    },
+    {
+      id: "3",
+      img: "https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/blog-img6.jpg",
+      date: "August 6, 2021",
+      category: "Construction",
+      title: "Planning Point Of View",
+      text: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi!",
+    },
+    {
+      id: "4",
+      img: "https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/blog-img6.jpg",
+      date: "August 6, 2021",
+      category: "Construction",
+      title: "Planning Point Of View",
+      text: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi!",
+    },
+    {
+      id: "5",
+      img: "https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/blog-img6.jpg",
+      date: "August 6, 2021",
+      category: "Construction",
+      title: "Planning Point Of View",
+      text: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi!",
+    },
+    {
+      id: "6",
+      img: "https://globefarer.qodeinteractive.com/wp-content/uploads/2021/08/blog-img6.jpg",
+      date: "August 6, 2021",
+      category: "Construction",
+      title: "Planning Point Of View",
+      text: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas ipsum tenetur optio suscipit totam eos laborum, quos provident eum modi!",
+    },
+   
+  
+  ]
