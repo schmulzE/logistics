b78c97913738479308fae3ab0587c66978ac3846
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-09-05 21:54:31 +0100

    fixed smooth transition of tabs using gsap

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useState, useRef, useLayoutEffect } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { TabList } from '../../data/tab'
@@ -11,18 +11,56 @@ const Tab = () => {
   const [openTab, setOpenTab] = useState(1)
   const revealRef = useRef<HTMLLIElement[]>([])
   const infoRef = useRef<HTMLDivElement[]>([])
+  const container = useRef<HTMLDivElement>(null)
   revealRef.current = []
   infoRef.current = []
-  
-  useEffect(() => {
-    revealRef.current.forEach((el, index) => {
-      el.addEventListener('click', () => {
-        infoRef.current.forEach((el: any, index) => {
-          // const tl = gsap.timeline()
-          const container = el.querySelector('.text-container');
-          const title = el.querySelector('.title');
-          const text = el.querySelector('.text');
-          const links = el.querySelector('.a');
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() =>{
+
+      const $slides: any[] = gsap.utils.toArray(".image-container");
+      const $slidesImage: any[] = gsap.utils.toArray(".img");
+      let totalSlides = $slides.length;
+      let currentSlide = 0;
+
+      const slidesWrap = gsap.utils.wrap(0, totalSlides);
+      const transitionInSlide = ({ slide , direction = 1, duration = 1 }: {slide: number, direction?: number, duration?: number}) => {
+      gsap.fromTo(
+          $slides[slide],
+          {
+              xPercent: direction > 0 ? 100 : -100
+        },
+        {
+          xPercent: 0,
+          duration
+        }
+      );
+        
+      gsap.fromTo(
+        $slidesImage[slide],
+        {
+            xPercent: direction > 0 ? -100 : 100
+          },
+          {
+              xPercent: 0,
+              duration
+            }
+          );
+        }
+        
+      const clickTab = (index: number) => {
+          currentSlide = slidesWrap(index);
+          transitionInSlide({ slide: currentSlide });
+        }
+          
+      for(let i = 0; i < revealRef.current.length; i++) {
+        revealRef.current[i].addEventListener("click", () => {
+          clickTab(i)
+          const container = infoRef.current[i].querySelector('.text-container');
+          const title = infoRef.current[i].querySelector('.title');
+          const text = infoRef.current[i].querySelector('.text');
+          const links = infoRef.current[i].querySelector('.a');
+          const tl = gsap.timeline({})
           gsap.fromTo(container, 
             {autoAlpha: 0, 
               x: -30
@@ -31,7 +69,6 @@ const Tab = () => {
               x: 0,
               duration: 1,
               autoAlpha: 1,
-              // ease: "Power2.easeInOut",
             })
           gsap.fromTo(title, 
             {autoAlpha: 0, 
@@ -44,31 +81,32 @@ const Tab = () => {
               ease: "Power2.easeInOut",
               delay: 0.2
             })
-          gsap.fromTo(text, 
-            {autoAlpha: 0, 
-              x: -80
-            },
-            {
-              x: 0,
-              duration: 0.8,
-              autoAlpha: 1,
-              ease: "Power2.easeInOut",
-              delay: 0.3
+            gsap.fromTo(text, 
+              {autoAlpha: 0, 
+                x: -80
+              },
+              {
+                x: 0,
+                duration: 0.8,
+                autoAlpha: 1,
+                ease: "Power2.easeInOut",
+                delay: 0.3
+              })
+              gsap.fromTo(links, 
+                {autoAlpha: 0, 
+                  x: -80
+                },
+                {
+                  x: 0,
+                  duration: 0.8,
+                  autoAlpha: 1,
+                  ease: "Power2.easeInOut",
+                  delay: 0.4
+                })
             })
-          gsap.fromTo(links, 
-            {autoAlpha: 0, 
-              x: -80
-            },
-            {
-              x: 0,
-              duration: 0.8,
-              autoAlpha: 1,
-              ease: "Power2.easeInOut",
-              delay: 0.4
-            })
-          })
-      })
-    })
+          }
+        }, container);
+        return () => ctx.revert();
   }, [])
 
   const tabListRef = (el: HTMLLIElement) => {
@@ -84,7 +122,7 @@ const Tab = () => {
   
 
   return (
-    <div id="section-3" className='bg-[#fff] lg:px-32 px-16 py-36' >
+    <div id="section-3" className='bg-[#fff] lg:px-32 px-8 lg:py-36 py-16' ref={container} >
       <div className="flex flex-wrap gap-3">
        <div className="w-full relative">
           <ul
@@ -92,7 +130,7 @@ const Tab = () => {
             role="tablist"
             >
             {TabList.map((item) => ( 
-            <li key={item.id} ref={tabListRef} className={"-mb-px last:mr-0 lg:flex-auto lg:text-center " + (openTab !== item.id && `${styles["tab-link"]}`)}>
+            <li key={item.id} ref={tabListRef} className={"-mb-px last:mr-0 lg:flex-auto lg:text-center" + (openTab !== item.id && `${styles["tab-link"]}`)}>
               <a
                 className={
                   "text-sm md:text-lg font-medium lg:px-5 py-4 cursor-none block leading-normal" +
@@ -120,9 +158,11 @@ const Tab = () => {
                {TabList.map((item) => ( 
                   <div key={item.id} ref={divsRef} className={openTab === item.id ? "block" : "hidden"} id="link">
                   <div className="lg:grid grid-cols-2 gap-10 mt-8">
-                     <div className="image-container">
-                        <img src={item.image} width={800}/>
-                     </div>
+                    <div id={`${styles.slider}`}>
+                      <div className={`image-container ${styles.slide}`}>
+                          <img src={item.image} width={800} className='img'/>
+                      </div>
+                    </div>
                      <div className="text-container lg:px-16">
                         <h1  className="text-3xl font-bold mb-5 title mt-8 lg:mt-0">Secure courier<br/> for small & big packages</h1>
                         <p className='mb-5 text'>{item.text}</p>
