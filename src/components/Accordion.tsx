945b98f0caa13b889e6ec56895a0e18e46bcde89
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-09-05 22:01:19 +0100

    added css classes to make component responsive

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -8,7 +8,6 @@ type AccordionData = {
 }
 const Accordion = ({ section, key } : { section: AccordionData, key: number }) => {
   const { isOpen, toggle } = useOpenController(false);
-  // console.log(section);
   return (
     <>
       <ExpendableColumn
@@ -25,9 +24,9 @@ const Accordion = ({ section, key } : { section: AccordionData, key: number }) =
 
 export const ExpendableColumn = ({ service, solution, isOpen, toggle } : { service: string, solution: string, isOpen: boolean, toggle: () => void }) => {
   return (
-    <div className="grid grid-cols-12 py-12 " onClick={toggle}>
-      <div className="col-span-4 font-medium text-xl capitalize">{service}</div>
-      <div className="col-span-8 a text-base flex gap-12 text-gray ml-4">
+    <div className="lg:grid lg:grid-cols-12 grid-cols-1 py-12 " onClick={toggle}>
+      <div className="lg:col-span-4 font-medium text-xl capitalize">{service}</div>
+      <div className="lg:col-span-8 a text-base flex gap-12 text-gray lg:ml-4">
         {solution}
       <button className="text-black">
        <div className="text-xl bg-[#39ff14] rounded-full w-8 h-8 cursor-none" >
@@ -42,8 +41,8 @@ export const ExpendableColumn = ({ service, solution, isOpen, toggle } : { servi
 
 export const TextSection = ({ text } : { text: string }) => {
   return (
-    <div className="text-gray my-4 grid grid-cols-12 gap-10">
-      <div className="col-start-5 col-end-12" >{text}</div>
+    <div className="text-gray my-4 lg:grid lg:grid-cols-12 lg:gap-10">
+      <div className="lg:col-start-5 lg:col-end-12" >{text}</div>
     </div>
   );
 };
