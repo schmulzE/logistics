070d692e82cbe2ca6d6a28fd49776a476cb921a6
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-09-05 21:48:55 +0100

    implemented sticky navbar logic

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,37 @@
-import React, { Dispatch } from 'react'
+import React, { Dispatch, useEffect, useState } from 'react'
 import Footer from '../components/Footer/Footer'
 import Navbar from '../components/Navbar/Navbar'
+import Navbar2 from '../components/Navbar/Navbar2'
 import styles from './Layout.module.css'
 import { Outlet } from 'react-router-dom'
 
 
-const Layout = ({setIsInverted, stickyClass, setStickyClass} : {setIsInverted: Dispatch<boolean>, stickyClass: string, setStickyClass: Dispatch<string>}) => {
+const Layout = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    // Function to handle scroll events
+    const handleScroll = () => {
+      if (window.scrollY > 600) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    // Add the event listener when the component mounts
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
   return (
     <div className={ styles.layout }>
-      <Navbar setIsInverted={setIsInverted} stickyClass={stickyClass} setStickyClass={setStickyClass}/>
+      {scrolling ? <Navbar2/> : <Navbar/>}
         <main>
           <Outlet/>
         </main>
